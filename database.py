a7b6cf7d529d9651ab6838ec7084d69ab3fcd162
chen <2625520488@qq.com>
2022-10-10 12:32:11 +0800

    数据库

---
+++ database.py
@@ -0,0 +1,35 @@
+from sqlalchemy import *
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+HOSTNAME = '127.0.0.1'
+DATABASE = 'flask'
+PORT = 3306
+USERNAME = 'root'
+PASSWORD = 'Apromise202077+'
+DB_URL = 'mysql+pymysql://{}:{}@{}:{}/{}'.format(USERNAME,PASSWORD,HOSTNAME,PORT,DATABASE)
+engine = create_engine(DB_URL)
+
+Base = declarative_base(engine)
+
+class Person(Base):
+    # 创建表结构操作
+    # 表名
+    __tablename__ = 'user'
+    #  字段
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(50), nullable=False)
+    gender = Column(Integer, default=0, comment="1男，2女")
+    pnumber =(Column(String(50), nullable=False))
+class Photos(Base):
+    # 创建表结构操作
+    # 表名
+    __tablename__ = 'uphotos'
+    #  字段
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    url = Column(String(50), nullable=False)
+Base.metadata.create_all()
+# 创建一个会话
+session = sessionmaker(engine)()
+p1 = Person(name='张三', gender='1',pnumber='1234')
+session.add(p1)
+session.commit()
