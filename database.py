13c4da2b961155b0b0995dcc8d11de7468224241
chen <2625520488@qq.com>
2022-10-24 22:51:30 +0800

    更改了数据库和文件上传

--- database.py
+++ database.py
@@ -2,11 +2,26 @@ from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from watchlist import db,app
 class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
+    __tablename__ = 'user'
+    __table_args__ = {'extend_existing': True}
+    id = db.Column(db.Integer, primary_key=True,autoincrement=True)
     name = db.Column(db.String(20))
     username = db.Column(db.String(20))
     password = db.Column(db.String(128))
+    def __init__(self):
+        self.username=None
+        self.id=None
+        self.username=None
+        self.password=None
 
+    def is_authenticated(self):
+        return True
+    def is_active(self):
+        return True
+    def is_anonymous(self):
+        return False
+    def get_id(self):
+        return self.id
     # def set_password(self, password):
     #     self.password_hash = generate_password_hash(password)
     #
@@ -16,10 +31,15 @@ class Photos(db.Model):
     # 创建表结构操作
     # 表名
     __tablename__ = 'uphotos'
+    __table_args__ = {'extend_existing': True}
     #  字段
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     uid = db.Column(db.Integer,db.ForeignKey("user.id"))
-    url = db.Column(db.String(50), nullable=False)
+    url = db.Column(db.String(100), nullable=False)
+    def __init__(self):
+        self.id=None
+        self.uid=None
+        self.url=None
 # with app.app_context():
 #     # db.init_app(app)
 #     db.drop_all()
--- upload.py
+++ upload.py
@@ -1,31 +1,42 @@
+from flask import session
 from flask import Flask, render_template, request, redirect, url_for, make_response, jsonify
 from werkzeug.utils import secure_filename
 import os
 from watchlist import app,db
 from datetime import timedelta
+from database import User,Photos
+from flask_login import login_required,current_user
+from sqlalchemy.orm import sessionmaker
 
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, DateTime
 
+#判断允许上传的文件类型
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1] in (['png', 'jpg', 'JPG', 'PNG', 'gif', 'GIF'])
+
 @app.route('/upload', methods=['GET', 'POST'])
+@login_required
 def upload():
     if request.method == 'POST':
         f = request.files['file']
-
         if not (f and allowed_file(f.filename)):
             return jsonify({"error": 1001, "msg": "请检查上传的图片类型，仅限于png、PNG、jpg、JPG、bmp"})
-
         user_input = request.form.get("name")
-
         basepath = os.path.dirname(__file__)  # 当前文件所在路径
-
         upload_path = os.path.join(basepath, 'static/photo', secure_filename(f.filename))  # 注意：没有的文件夹一定要先创建，不然会提示没有该路径
         f.save(upload_path)
 
-        # image_data = open(upload_path, "rb").read()
-        # response = make_response(image_data)
-        # response.headers['Content-Type'] = 'image/png'
-        # return response
-
+        #在成功上传图片后，上传图片的信息到数据库
+        photo=Photos()
+        photo.uid=current_user.id
+        photo.url=upload_path
+        db.session.add(photo)
+        db.session.commit()
     return render_template('upload.html')
+
+
+
+
+
 
